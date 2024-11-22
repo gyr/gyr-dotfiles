391401ddc9ea949fcb8ec7408b35a759e38f7e72
Gustavo Yokoyama Ribeiro <gyribeiro@suse.com>
2024-11-22 08:46:39 -0300

    Cleanup, update and reorganize config files
    - Delete unused configs
    - Reorganize conky configs
    - Update git hooks to python3

--- git_template/hooks/pylint.py
+++ git_template/hooks/pylint.py
@@ -1,5 +1,5 @@
 import sys
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, DEVNULL
 
 
 def exec_cmd(cmd):
@@ -10,14 +10,14 @@ def exec_cmd(cmd):
     :returns: tuple(str, int) command output, return code
 
     """
-    proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, encoding='utf-8')
     output, error = proc.communicate()
     if proc.returncode != 0:
-        print "FAILED to execute:"
-        print str(cmd)
-        print "Error message:"
-        print str(error)
-        print str(output)
+        print("FAILED to execute:")
+        print(cmd)
+        print("Error message:")
+        print(error)
+        print(output)
     return output, proc.returncode
 
 
@@ -26,17 +26,21 @@ def main():
     Check commited python files with Pylint!
 
     """
-    changed_files, ret_code = exec_cmd("git diff --staged --name-only HEAD")
+    changed_files, ret_code = exec_cmd("git diff --cached --name-only HEAD")
     python_files = []
-    for f in changed_files.split():
+    for f in changed_files.splitlines():
+        f = f.strip()
+        if not f:
+            continue
         file_type, ret_code = exec_cmd("file {}".format(f))
         if "Python script" in file_type or f.endswith(".py"):
             python_files.append(f)
 
     exit_code = 0
     for f in python_files:
-        _, ret_code = exec_cmd("pylint -E {}".format(f))
+        out, ret_code = exec_cmd("pylint -E {} 2>&1".format(f))
         if ret_code != 0:
+            print(out)
             exit_code = 1
     sys.exit(exit_code)
 
