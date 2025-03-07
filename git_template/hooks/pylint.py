52f20056d2baa8073e6aa787c5e7d1a814515ef4
Gustavo Yokoyama Ribeiro <gyribeiro@suse.com>
2025-03-07 17:09:19 -0300

    Update pylint hook
    - Refactor
    - Address issue when file is rename and pylint runs on the old filename

--- git_template/hooks/pylint.py
+++ git_template/hooks/pylint.py
@@ -1,8 +1,9 @@
+import os
 import sys
-from subprocess import Popen, PIPE, DEVNULL
+from subprocess import Popen, PIPE
 
 
-def exec_cmd(cmd):
+def exec_cmd(cmd: str) -> tuple[str, int]:
     """
     Execute external command (shell)
 
@@ -10,37 +11,38 @@ def exec_cmd(cmd):
     :returns: tuple(str, int) command output, return code
 
     """
-    proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, encoding='utf-8')
-    output, error = proc.communicate()
-    if proc.returncode != 0:
-        print("FAILED to execute:")
-        print(cmd)
-        print("Error message:")
-        print(error)
-        print(output)
-    return output, proc.returncode
+    with Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, encoding='utf-8') as proc:
+        output, error = proc.communicate()
+        if proc.returncode != 0:
+            print("FAILED to execute:")
+            print(cmd)
+            print("Output message:")
+            print(output)
+            print("Error message:")
+            print(error)
+        return (output, proc.returncode)
 
 
-def main():
+def main() -> None:
     """
     Check commited python files with Pylint!
 
     """
-    changed_files, ret_code = exec_cmd("git diff --cached --name-only HEAD")
-    python_files = []
+    changed_files, _ = exec_cmd("git diff --cached --name-only HEAD")
+    python_files: list[str] = []
     for f in changed_files.splitlines():
         f = f.strip()
         if not f:
             continue
-        file_type, ret_code = exec_cmd("file {}".format(f))
-        if "Python script" in file_type or f.endswith(".py"):
-            python_files.append(f)
+        if os.path.exists(f) and os.path.isfile(f):
+            file_type, _ = exec_cmd(f"file {f}")
+            if "Python script" in file_type or f.endswith(".py"):
+                python_files.append(f)
 
-    exit_code = 0
+    exit_code: int = 0
     for f in python_files:
-        out, ret_code = exec_cmd("pylint -E {} 2>&1".format(f))
+        _, ret_code = exec_cmd(f"pylint -E {f} 2>&1")
         if ret_code != 0:
-            print(out)
             exit_code = 1
     sys.exit(exit_code)
 
